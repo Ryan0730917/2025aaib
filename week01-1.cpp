f6573f030eb275f6967af7588ac9cec795e6398f
Ryan0730917 <lij71610@gmail.com>
2025-02-19 11:55:16 +0800

    week01-1

---
+++ week01-1.cpp
@@ -0,0 +1,15 @@
+/// week01-1.cpp瘋狂程設 兩樹相加
+
+#include <stdio.h>
+///stido的意思是標準的standard input/output
+///有了stdio.h的外掛,才能用scznf() printf()
+int main()
+{
+	int a, b;
+	scanf( "%d" , &a );
+	scanf( "%d" , &b );
+
+	printf("%d", a+b);
+
+}
+
